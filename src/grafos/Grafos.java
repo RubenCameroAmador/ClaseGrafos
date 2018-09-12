7f4aad6107f6e587828f1640b84f16da5543f1c5
ESTU-5K <ESTU-5K@10.20.21.161>
2018-09-12 15:55:57 -0500

    Cree los métodos para mostrar y los recorridos de la lista de adyacencia

--- src/grafos/Grafos.java
+++ src/grafos/Grafos.java
@@ -17,10 +17,10 @@ public class Grafos {
     static LinkedList<Vertice> grafo = new LinkedList<Vertice>();
 
     static int[][] M = {{0, 1, 1, 0, 1},
-    {1, 0, 0, 1, 1},
-    {1, 1, 0, 1, 1},
-    {0, 1, 1, 0, 0},
-    {1, 1, 1, 0, 0}};
+                        {1, 0, 1, 1, 1},
+                        {1, 1, 0, 1, 1},
+                        {0, 1, 1, 0, 0},
+                        {1, 1, 1, 0, 0}};
     //DFS: 01234
     //BFS: 01243
 
@@ -36,20 +36,51 @@ public class Grafos {
             grafo.add(temp);
         }
     }
+    static void showList(){
+        for (Vertice v : grafo) {
+            System.out.print(v.getDato()+"=> ");
+            for (Integer x : v.getAdyacentes()) {
+                System.out.print(x+" ");
+            }
+            System.out.println("");
+        }
+    }
 
     static boolean[] visitado = new boolean[M.length];
 
-    public static void DFS(int u) {
-        System.out.print(u + " ");
-        visitado[u] = true;
-        for (int v = 0; v < M.length; v++) {
-            if (M[u][v] == 1) {
-                if (!visitado[v]) {
+   public static void DFS(int u){
+        System.out.print(u+" ");
+        visitado[u]=true;
+        for (int v = 0; v < M.length; v++) 
+            if (M[u][v]==1) 
+                if (!visitado[v]) 
                     DFS(v);
-                }
-            }
-        }
     }
+   
+   public static void DFSList(int u){
+       Vertice a = grafo.get(u);
+       System.out.print(a.getDato());
+       visitado[u]=true;
+       for (int i = 0; i < a.totalAdyacente(); i++) 
+           if(!visitado[i])
+               DFSList(i);
+   }
+   public static void BFSList(int u){
+       Queue<Integer> cola = new LinkedList<Integer>();
+       visitado[u]=true;
+       cola.add(u);
+       while(!cola.isEmpty()){
+           u = cola.remove();
+           System.out.print(u+" ");
+           Vertice a = grafo.get(u);
+           for (int i = 0; i < a.totalAdyacente(); i++) {
+               if(!visitado[u]){
+                   visitado[i]=true;
+                   cola.add(i);
+               }
+           }
+       }
+   }
 
     public static void BFS(int u) {
         Queue<Integer> cola = new LinkedList<Integer>(); //FIFO
@@ -67,7 +98,6 @@ public class Grafos {
                 }
             }
         }
-
     }
 
     /**
@@ -75,14 +105,27 @@ public class Grafos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("DFS: ");
+        System.out.println("DFS:");
         DFS(0);
-        for (int i = 0; i < visitado.length; i++) {
-            visitado[i] = false;
-        }
+        
+        for (int i = 0; i < visitado.length; i++) 
+            visitado[i]=false;
         System.out.println("\n\rBFS:");
         BFS(0);
+        
+        System.out.println("\n\rA partir  de aquí. Comienza lo de lista");
         llenarLista();
+        showList();
+        
+        for (int i = 0; i < visitado.length; i++) 
+            visitado[i]=false;
+        DFSList(0);
+        
+        System.out.println("\n\rAhora viene el BFS de la lista");
+        for (int i = 0; i < visitado.length; i++) 
+            visitado[i]=false;
+        BFSList(0);
+        
     }
 
 }
